958ca89313c82227ae5249cebbaeed97cdfa5750
Larissa de Souza Costa <120164725+vulpeslari@users.noreply.github.com>
2023-03-25 17:04:36 -0400

    Delete robotron directory

--- robotron/config.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from arena import Arena
-pygame.init()
-pygame.font.init()
-sc_width = 1300
-sc_height = 800
-screen = pygame.display.set_mode((sc_width, sc_height))
-velocity = 5
--- robotron/sprites.py
+++
@@ -1,5 +0,0 @@
-import pygame
-boy = []
-for i in range(1, 13):
-    boy.append(pygame.image.load(f"boy/boy-{i}.png"))
-print(boy)
